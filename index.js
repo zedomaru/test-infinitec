5a8b97c1810527ef6d55b9f36740a9d69dacc5ef
Jayakusumah <lukmanindrajayakusumah@gmail.com>
2020-09-11 13:28:27 +0700

    Add tugas.txt

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const utils = require('./utils');
 const { test1, test2 } = require('./test');
-
-console.log('test 1 result =>', utils.test1(test1))
-console.log('test 2 result =>', utils.test2(test2))
